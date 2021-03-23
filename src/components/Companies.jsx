d5fb5bf02ddb8981c8afee8b783089fd35d8b29c
Milande Teisserenc bonesteve <milandejf@gmail.com>
2021-03-23 13:43:38 +0100

    add infinite scroller

--- src/components/Companies.jsx
+++ src/components/Companies.jsx
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
 import Company from "./Company/Company";
+import "./Companies.css";
+import InfiniteScroll from "react-infinite-scroller";
 import axios from "axios";
 
 class Companies extends Component {
   state = { data: [] };
-  token = "lbBbl_IM7jicWjxsNrdQTrVmXJ8";
+  token = "uVJaLeIvXv1kF4Ub2iBCiPtB6vA";
 
-  componentDidMount() {
+  // function called by the infite loader component with the correct page number as parameter
+  getData = (page) => {
     axios
       .get(
-        "https://api.emploi-store.fr/partenaire/labonneboite/v1/company/?latitude=48.856614&longitude=2.3522219&rome_codes=M1805&sort=score&page_size=10&page=1",
+        `https://api.emploi-store.fr/partenaire/labonneboite/v1/company/?latitude=48.856614&longitude=2.3522219&rome_codes=M1805&sort=score&page_size=10&page=${page}`,
         {
           headers: {
             Authorization: "Bearer " + this.token, //the token is a variable which holds the token
@@ -17,9 +20,13 @@ class Companies extends Component {
         }
       )
       .then((res) => {
-        const data = res.data.companies.sort((a, b) => {
+        const newList = res.data.companies.sort((a, b) => {
           return b.stars - a.stars;
         });
+
+        // append the new list to the previous state of data
+        // in order to display all the data
+        const data = this.state.data.concat(newList);
         this.setState({
           data,
         });
@@ -28,7 +35,7 @@ class Companies extends Component {
       .catch((error) => {
         alert("Merci de rafraichir le token");
       });
-  }
+  };
 
   render() {
     return (
@@ -36,10 +43,23 @@ class Companies extends Component {
         <div>
           <p>Le top 10 des entreprises qui recherche un Développeur</p>
         </div>
-        <div>
-          {this.state.data.map((company) => {
-            return <Company company={company} />;
-          })}
+        <div></div>
+        <div style={{ height: "800px", overflow: "auto" }}>
+          <InfiniteScroll
+            pageStart={0}
+            loadMore={this.getData}
+            hasMore={true}
+            loader={
+              <div className="loader" key={0}>
+                <div class="lds-dual-ring"></div>
+              </div>
+            }
+            useWindow={false}
+          >
+            {this.state.data.map((company) => {
+              return <Company company={company} />;
+            })}
+          </InfiniteScroll>
         </div>
       </div>
     );
